ecc29eb9303254c68ef9cc2df1b12f6d361643e6
barel <barel.levy@mail.huji.ac.il>
2017-09-24 14:49:30 +0300

    added get_features

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 # CONSTANTS
 MSG="Given a folder with BAM files find windows around the TTS.\nFormat: sh main.sh <input folder>"
-
+SCRIPTS="make_bedtools.sh make_features.sh"
 if [ $# -eq 0 ]
 then
 	echo $MSG
@@ -11,3 +11,12 @@ fi
 
 FOLDER=$1 # the working directory
 JUMP=20 # how close must to windows be to be merged
+
+# make sure we can run all our files
+chmod 777 $SCRIPTS
+
+# makes the global window and local graph like file for each BAM file
+./make_bedtools.sh $FOLDER $JUMP
+
+# make a global file and a local file for each original BAM file with features
+./make_features.sh
